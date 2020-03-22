ea799bd1f6ab6901e9c8a6b34d4b380f3cf60f02
feixiaochun <feixiaochun@daihoubang.com>
2020-03-22 14:33:38 +0800

    优化环境配置文件

--- Config/env_config.py
+++ Config/env_config.py
@@ -1,37 +1,40 @@
 # -*- coding:utf-8 -*-
 
-# 邮箱配置参数
-ERROR_MAIL_HOST = "smtp.163.com"
-ERROR_MAIL_ACCOUNT = "miclloo@163.com"
-ERROR_MAIL_PASSWD = "qweasd123"  # 客户端授权密码，非登录密码
-
 # 日志、报告、截图 等路径
 LOGS_PATH = "/Users/micllo/Documents/works/GitHub/pythonSelenium/Logs/"
 REPORTS_PATH = "/Users/micllo/Documents/works/GitHub/pythonSelenium/Reports/"
 SCREENSHOTS_PATH = "/Users/micllo/Documents/works/GitHub/pythonSelenium/Screenshots/"
 
-# 构建的时候使用前端静态文件路径 ( Api/__init__.py文件的同级目录 )
-# GULP_STATIC_PATH = 'static'
-# GULP_TEMPLATE_PATH = 'templates'
-GULP_STATIC_PATH = '../Build'
-GULP_TEMPLATE_PATH = '../Build/templates'
-
 # 服务器地址
 SERVER_IP = "127.0.0.1"
 
+# Nginx 端口
+NGINX_PORT = "8070"
+
+# Nginx中的接口反向代理名称
+NGINX_API_PROXY = "api_local"
+
 # 是否启用远程浏览器
-REMOTE = True
+REMOTE = False
+
+############################################# 相 同 的 配 置 #############################################
+
+# 邮箱配置参数
+ERROR_MAIL_HOST = "smtp.163.com"
+ERROR_MAIL_ACCOUNT = "miclloo@163.com"
+ERROR_MAIL_PASSWD = "qweasd123"  # 客户端授权密码，非登录密码
+
+# 构建的时候使用前端静态文件路径 ( Api/__init__.py文件的同级目录 ) 'static'、'templates'
+GULP_STATIC_PATH = '../Build'
+GULP_TEMPLATE_PATH = '../Build/templates'
 
 # 测试报告地址
-BASE_REPORT_PATH = "http://" + SERVER_IP + ":8070/test_report_local/"
+BASE_REPORT_PATH = "http://" + SERVER_IP + ":" + NGINX_PORT + "/test_report_local/"
 CURRENT_REPORT_URL = BASE_REPORT_PATH + "report.html"
 HISTORY_REPORT_PATH = BASE_REPORT_PATH + "history/"
 
-# nginx中的接口方向代理名称
-NGINX_API_PROXY = "api_local"
-
 # 接口地址( uwsgi )
-API_ADDR = SERVER_IP + ":8070/" + NGINX_API_PROXY
+API_ADDR = SERVER_IP + ":" + NGINX_PORT + "/" + NGINX_API_PROXY
 
 # Selenium Grid Console
 # GRID_REMOTE_ADDR = "10.211.55.6:4444"  # win10虚拟机
@@ -48,4 +51,3 @@ MAIL_LIST = ["micllo@126.com"]
 DD_MONITOR_GROUP = "3a2069108f0775762cbbfea363984c9bf59fce5967ada82c78c9fb8df354a624"
 DD_AT_PHONES = "13816439135,18717854213"
 DD_AT_FXC = "13816439135"
-
--- Config/env_config_docker.py
+++ Config/env_config_docker.py
@@ -1,36 +1,41 @@
 # -*- coding:utf-8 -*-
 
-# 邮箱配置参数
-ERROR_MAIL_HOST = "smtp.163.com"
-ERROR_MAIL_ACCOUNT = "miclloo@163.com"
-ERROR_MAIL_PASSWD = "qweasd123"  # 客户端授权密码，非登录密码
-
 # 日志、报告、截图 等路径
 LOGS_PATH = "/opt/project/logs/"
 REPORTS_PATH = "/opt/project/reports/"
 SCREENSHOTS_PATH = "/opt/project/screenshots/"
 
-# 构建的时候使用前端静态文件路径 ( Api/__init__.py文件的同级目录 )
-GULP_STATIC_PATH = '../Build'
-GULP_TEMPLATE_PATH = '../Build/templates'
-
 # 服务器地址
 # SERVER_IP_PORT = "192.168.3.102"  #  MERCURY_1602
 SERVER_IP = "192.168.31.10"  # Demba Ba_5G
 
+# Nginx 端口
+NGINX_PORT = "1080"
+
+# Nginx中的接口反向代理名称
+NGINX_API_PROXY = "api"
+
 # 是否启用远程浏览器
 REMOTE = True
 
+############################################# 相 同 的 配 置 #############################################
+
+# 邮箱配置参数
+ERROR_MAIL_HOST = "smtp.163.com"
+ERROR_MAIL_ACCOUNT = "miclloo@163.com"
+ERROR_MAIL_PASSWD = "qweasd123"  # 客户端授权密码，非登录密码
+
+# 构建的时候使用前端静态文件路径 ( Api/__init__.py文件的同级目录 )
+GULP_STATIC_PATH = '../Build'
+GULP_TEMPLATE_PATH = '../Build/templates'
+
 # 测试报告地址
-BASE_REPORT_PATH = "http://" + SERVER_IP + ":1080/test_report/"
+BASE_REPORT_PATH = "http://" + SERVER_IP + ":" + NGINX_PORT + "/test_report/"
 CURRENT_REPORT_URL = BASE_REPORT_PATH + "report.html"
 HISTORY_REPORT_PATH = BASE_REPORT_PATH + "history/"
 
-# nginx中的接口方向代理名称
-NGINX_API_PROXY = "api"
-
 # 接口地址
-API_ADDR = SERVER_IP + ":1080/" + NGINX_API_PROXY
+API_ADDR = SERVER_IP + ":" + NGINX_PORT + "/" + NGINX_API_PROXY
 
 # Selenium Grid Console
 # GRID_REMOTE_ADDR = "10.211.55.6:4444"  # win10虚拟机
@@ -47,4 +52,3 @@ MAIL_LIST = ["micllo@126.com"]
 DD_MONITOR_GROUP = "3a2069108f0775762cbbfea363984c9bf59fce5967ada82c78c9fb8df354a624"
 DD_AT_PHONES = "13816439135,18717854213"
 DD_AT_FXC = "13816439135"
-
